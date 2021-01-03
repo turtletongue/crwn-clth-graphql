eea640ff1a067b56fc98acce6c7b25445bbc3ce1
platon <platon.s14@list.ru>
2021-01-03 20:11:36 +0500

    Завершение проекта.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
 import CollectionPreview from '../collection-preview/collection-preview.component';
-
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
-
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
@@ -16,8 +10,4 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
